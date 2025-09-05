7052403b7d67d2530d0b74f0cf3b51bc5845a95d
entlein <einentlein@gmail.com>
2025-09-05 19:15:10 +0200

    feature: a lot more globbing and adding the first set of init containers
    Signed-off-by: entlein <einentlein@gmail.com>

--- testdata/superset.sh
+++ testdata/superset.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 INPUT_DIR="$1"
-OUTPUT_FILE="$2"
+OUTPUT_DIR="$1"
 
-if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_FILE" ]; then
+if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
   echo "Usage: $0 <directory_with_bobs> <output_superset_file.yaml>"
   exit 1
 fi
@@ -18,11 +18,25 @@ if [ ! -d "$INPUT_DIR" ]; then
     exit 1
 fi
 
-FILES=("$INPUT_DIR"/*.yaml)
-if [ ${#FILES[@]} -eq 0 ]; then
-  echo "No YAML files found in $INPUT_DIR"
-  exit 1
-fi
+
+declare -A GROUPSS
+
+for file in "$INPUT_DIR"/*.yaml; do
+  [ -f "$file" ] || continue
+  name=$(yq '.metadata.name' "$file")
+  echo $name
+  shortname=$(echo "$name" | sed -E 's/-[0-9a-f]{6,}$//')   
+  echo $shortname
+  GROUPSS["$shortname"]+="$file "
+done
+
+
+for shortname in "${!GROUPSS[@]}"; do
+  echo "Processing group: $shortname"
+
+  FILES=(${GROUPSS[$shortname]})
+
+
 
 all_syscalls=()
 all_capabilities=()
@@ -45,6 +59,7 @@ normalize_opens() {
   )
   | unique_by(.path + ( .flags | tostring ))'
 }
+
 
 collapse_opens_with_globs() {
   jq '
@@ -93,20 +108,24 @@ for file in "${FILES[@]}"; do
       .metadata.uid,
       .metadata.labels."kubescape.io/workload-resource-version",
       .metadata.annotations."kubescape.io/resource-size"
-    ) |
-    .spec.containers[0].capabilities |= (. // [] | sort | unique) |
-    .spec.containers[0].syscalls |= (. // [] | sort | unique) |
-    .spec.containers[0].execs |= (. // [] | unique_by(.path)) |
-    .spec.containers[0].opens |= (
+    )'  "$file" > "$norm_file"
+
+  yq eval '
+    .spec.containers[0]? |= (
+    .capabilities |= (. // [] | sort | unique) |
+    .syscalls |= (. // [] | sort | unique) |
+    .execs |= (. // [] | unique_by(.path)) |
+    .opens |= (
       . // []
-      | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "⋯"))   # normalize pod UID
-      | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "⋯.scope")) # normalize container ID
+      | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "⋯"))   
+      | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "⋯.scope")) 
       | map(.path |= sub("\\.\\.[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\\.[0-9]+", "⋯"))
       | unique_by(.path)
     ) |
-    .spec.containers[0].endpoints |= (. // [] | sort | unique)|
-    .spec.containers[0].rules |= (. // [] | sort | unique)
-  ' "$file" > "$norm_file"
+    .endpoints |= (. // [] | sort | unique)|
+    .rules |= (. // [] | sort | unique)
+    )
+  ' -i "$norm_file"
 
   syscalls=$(yq '.spec.containers[0].syscalls[]' "$norm_file" 2>/dev/null || true)
   capabilities=$(yq '.spec.containers[0].capabilities[]' "$norm_file" 2>/dev/null || true)
@@ -126,28 +145,26 @@ for file in "${FILES[@]}"; do
   name=$(yq '.metadata.name' "$norm_file" 2>/dev/null || true)
 
 
-  execs_json=$(yq -o=json '.spec.containers[0].execs' "$norm_file" 2>/dev/null || echo "[]")
-  opens_json=$(yq -o=json '.spec.containers[0].opens' "$norm_file" 2>/dev/null || echo "[]")
-  endpoints_json=$(yq -o=json '.spec.containers[0].endpoints' "$norm_file" 2>/dev/null || echo "[]")
-  rules_json=$(yq -o=json '.spec.containers[0].rulePolicies | select(. != null) | to_entries' "$norm_file" 2>/dev/null || echo "[]")
+  execs_json=$(yq -o=json '.spec.containers[].execs' "$norm_file" 2>/dev/null || echo "[]")
+  opens_json=$(yq -o=json '.spec.containers[].opens' "$norm_file" 2>/dev/null || echo "[]")
+  endpoints_json=$(yq -o=json '.spec.containers[].endpoints' "$norm_file" 2>/dev/null || echo "[]")
+  rules_json=$(yq -o=json '.spec.containers[].rulePolicies | select(. != null) | to_entries' "$norm_file" 2>/dev/null || echo "[]")
 
 
-
-  # Merge execs
   all_execs_json=$(jq -s 'add | unique_by(.path)' <(echo "$all_execs_json") <(echo "$execs_json"))
-  # Merge opens
   all_opens_json=$(jq -s 'add | unique_by(.path)' <(echo "$all_opens_json") <(echo "$opens_json"))
   all_opens_json=$(echo "$all_opens_json" | normalize_opens)
   all_opens_json=$(echo "$all_opens_json" | collapse_opens_with_globs|collapse_opens_events)
-  # Merge endpoints
   all_endpoints_json=$(jq -s 'add | unique' <(echo "$all_endpoints_json") <(echo "$endpoints_json"))
-  # Merge rules -> for each rule, we need to sort and unique them
   all_rules_json=$(jq -s 'add' <(echo "$all_rules_json") <(echo "$rules_json"))
-
 
 
   all_syscalls+=($syscalls)
   all_capabilities+=($capabilities)
+
+  init_json=$(yq -o=json '.spec.initContainers // []' "$norm_file" 2>/dev/null || echo "[]")
+  all_init_json=$(jq -s 'add' <(echo "$all_init_json") <(echo "$init_json"))
+
 
   rm "$norm_file"
 done
@@ -201,7 +218,7 @@ indent_execs() {
 
 indent_opens() {
   while IFS= read -r line; do
-    if [[ "$line" =~ "- flags" ]]; then
+    if [[ "$line" =~ "- args" || "$line" =~ "- flags" ]]; then
       echo "    $line"
     elif [[ "$line" =~ "path" ]]; then
       echo "    $line"  
@@ -222,7 +239,7 @@ indent_rules() {
 }
 
 
-
+OUTPUT_FILE="$OUTPUT_DIR/${shortname}_bob.yaml"
 
 cat <<EOF > "$OUTPUT_FILE"
 apiVersion: spdx.softwarecomposition.kubescape.io/v1beta1
@@ -264,9 +281,11 @@ $(echo "$superset_rules" | indent_rules)
         defaultAction: ""
     syscalls:
 $(for s in "${superset_syscalls[@]}"; do echo "    - $s"; done)
+  initContainers:
+$(echo "$all_init_json" | jq -r '.[]' | yq -P | sed 's/^/    /' |  sed -E 's/^      - /    - /' | sed -E 's/^          -/      -/')
 EOF
 
-OUTPUT_FILE="$OUTPUT_FILE".helm
+OUTPUT_FILE="$OUTPUT_DIR/${shortname}_bob.helm"
 
 cat << EOF >> "$OUTPUT_FILE"
 {{- if .Values.bob.create }}
@@ -318,3 +337,4 @@ EOF
 echo "Superset arrays written to '$OUTPUT_FILE'"
 #    rulePolicies:
 #$(for r in "${rules_json[@]}"; do echo "     $r"; done)
+done
