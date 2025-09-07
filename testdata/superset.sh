0f5313e476b08c6d1032d067d695a5f2e9c4eaca
entlein <einentlein@gmail.com>
2025-09-07 15:03:09 +0200

    fix: leakage between keys now expunged, but we are still loosing execs and opens, dont use yet
    Signed-off-by: entlein <einentlein@gmail.com>

--- testdata/superset.sh
+++ testdata/superset.sh
@@ -2,6 +2,7 @@
 
 bla= ###!/usr/bin/env bash
 set -u
+#set -xe
 
 INPUT_DIR="$1"
 OUTPUT_DIR="$1"
@@ -21,8 +22,6 @@ if [ ! -d "$INPUT_DIR" ]; then
     echo "Error: Directory not found: $INPUT_DIR"
     exit 1
 fi
-
-
 
 indent_endpoints() {
   while IFS= read -r line; do 
@@ -70,7 +69,6 @@ indent_rules() {
   done
 }
 
-# Print a YAML key as a list or null, inline
 print_yaml_list_or_null_inline() {
   local key="$1"
   shift
@@ -86,22 +84,43 @@ print_yaml_list_or_null_inline() {
 normalize_opens() {
   jq 'map(
     .path |= (
-      # normalize hashes / pod ids
-      gsub("[0-9a-f]{32,}"; "⋯") |
-      gsub("pod[0-9a-fA-F_\\-]+"; "⋯") |
-      gsub("cri-containerd-[0-9a-f]{64}\\.scope"; "⋯.scope") |
-      gsub("[0-9]+"; "⋯") |
+      gsub("[0-9a-f]{32,}"; "*") |
+      gsub("-[0-9a-fA-F_\\-]{4,}+"; "*") |
+      gsub("(/bin/)[^/]+.*"; "\\1*") |
+      gsub("[0-9a-f]{64}"; "*") |
       gsub("/[^/]+\\.service"; "/*.service") |
-      gsub("/[^/]+\\.socket"; "/*.socket")
+      gsub("/proc/[^/]+/task/[^/]+/fd.*"; "/proc/*/task/*/fd")
     )
   )
   | unique_by(.path + ( .flags | tostring ))'
 }
 
-
+# normalize_opens() {
+#   jq 'map(
+#     .path |= (
+#       gsub("[0-9a-f]{32,}"; "⋯") |
+#       gsub("pod[0-9a-fA-F_\\-]+"; "⋯") |
+#       gsub("/var/log/pods/[^/]+/"; "/var/log/pods/⋯/") |
+#       gsub("/[a-z0-9]{8,}(-[a-z0-9]{4,}){2,}/"; "/⋯/") |
+#       gsub("/[a-z0-9]{16,}/"; "/⋯/") |
+#       gsub("/pods/[^/]+/"; "/pods/⋯/")|
+#       gsub("cri-containerd-[0-9a-f]{64}\\.scope"; "⋯.scope") |
+#       gsub("/[^/]+\\.service"; "/*.service") |
+#       gsub("/[^/]+\\.socket"; "/*.socket")|
+#       gsub("/[^/]+\\.(log|out|err)$"; "/⋯.\\1")|
+#       gsub("[0-9]+"; "⋯")|
+#       gsub("⋯+/?"; "⋯/") |
+#       gsub("/[.-]+⋯"; "/⋯") |
+#       gsub("(/⋯)+/"; "/⋯/") |
+#       gsub("⋯+/?"; "⋯/") 
+#     )
+#   )
+#   | unique_by(.path + ( .flags | tostring ))'
+# }
 
 collapse_opens_with_globs() {
   jq '
+    . // [] |
     group_by(.path | split("/")[:-1] | join("/")) |
     map(
       if length > 3 then
@@ -123,6 +142,7 @@ collapse_opens_with_globs() {
 }
 collapse_opens_events() {
   jq '
+    . // [] |
     group_by(.path | split("/")[:-1] | join("/")) |
     map(
       if length > 1 then
@@ -136,7 +156,6 @@ collapse_opens_events() {
   '
 }
 
-
 ## this is the most outer loop - the grouploop
 
 declare -A GROUPSS
@@ -145,9 +164,9 @@ for file in "$INPUT_DIR"/*.yaml; do
   [ -f "$file" ] || continue
   [[ "$file" == *_bob.yaml ]] && continue
   name=$(yq '.metadata.name' "$file")
-  echo $name
+  #echo $name
   shortname=$(echo "$name" | sed -E 's/-[0-9a-f]{6,}$//')   
-  echo $shortname
+  #echo $shortname
   GROUPSS["$shortname"]+="$file "
 done
 
@@ -156,7 +175,12 @@ for shortname in "${!GROUPSS[@]}"; do
 
   FILES=(${GROUPSS[$shortname]})
 
-
+unset superset_capabilities superset_syscalls superset_execs superset_open superset_endpoints superset_rules
+unset superset_init_execs superset_init_open superset_init_endpoints superset_init_rules superset_init_syscalls superset_init_capabilities
+unset all_endpoints_json all_rules_json all_execs_json all_opens_json all_syscalls all_capabilities
+unset all_init_execs_json all_init_opens_json all_init_endpoints_json all_init_rules_json all_init_syscalls all_init_capabilities
+unset keys containerName imageID imageTag identifiedCallStacks
+unset initkeys init_imageID init_imageTag init_containerName init_identifiedCallStacks
 declare -A keys
 declare -A imageID
 declare -A imageTag
@@ -230,9 +254,9 @@ for file in "${FILES[@]}"; do
         .execs |= (. // [] | unique_by(.path)) |
         .opens |= (
           . // []
-          | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "⋯"))   
-          | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "⋯.scope")) 
-          | map(.path |= sub("\\.\\.[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\\.[0-9]+", "⋯"))
+          | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "*"))   
+          | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "*.scope")) 
+          | map(.path |= sub("\\.\\.[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\\.[0-9]+", "*"))
           | unique_by(.path)
         ) |
         .endpoints |= (. // [] | sort | unique)|
@@ -242,7 +266,7 @@ for file in "${FILES[@]}"; do
 
         #Assumption: containerName is hopefully unique across a deploymentspec
         containerName=$(yq -r ".spec.containers[$i].name // \"\" " "$norm_file" 2>/dev/null || true)
-        key="${containerName//-/_}" 
+        key="${shortname}${containerName}" 
         keys["$key"]="${key}"
         echo "Taking on container $i $workloadname $key"
 
@@ -298,78 +322,78 @@ for file in "${FILES[@]}"; do
   done #end of container(key)
 
   init_container_count=$(yq '.spec.initContainers // [] | length' "$norm_file")
-      for i in $(seq 0 $((init_container_count-1))); do
+      for j in $(seq 0 $((init_container_count-1))); do
         if [ "$init_container_count" -eq 0 ]; then
           continue
         fi
-      echo "Taking on initContainer $i" $(yq ".spec.initContainers[$i].imageID" "$norm_file" 2>/dev/null)
+      echo "Taking on initContainer $j $workloadname ${shortname}$(yq -r ".spec.initContainers[$j].name // \"\" " "$norm_file" 2>/dev/null || true)" 
           yq eval '
-            .spec.initContainers[$i] |= (
+            .spec.initContainers[$j] |= (
             .capabilities |= (. // [] | sort | unique) |
             .syscalls |= (. // [] | sort | unique) |
             .execs |= (. // [] | unique_by(.path)) |
             .opens |= (
               . // []
-              | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "⋯"))   
-              | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "⋯.scope")) 
-              | map(.path |= sub("\\.\\.[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\\.[0-9]+", "⋯"))
+              | map(.path |= sub("pod[0-9a-fA-F_\\-]+", "*"))   
+              | map(.path |= sub("cri-containerd-[0-9a-f]{64}\\.scope", "*.scope")) 
+              | map(.path |= sub("\\.\\.[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\\.[0-9]+", "*"))
               | unique_by(.path)
             ) |
             .endpoints |= (. // [] | sort | unique)|
             .rules |= (. // [] | sort | unique)
             )
           ' -i "$norm_file"
-        initcontainerName=$(yq -r ".spec.initContainers[$i].name // \"\" " "$norm_file" 2>/dev/null || true)
-        key="${initcontainerName//-/_}" 
-        echo $key "for initContainer"
-        : "${init_imageID["$key"]:=""}"
-        : "${init_imageTag["$key"]:=""}"
-        : "${init_containerName["$key"]:=""}"
-        : "${init_identifiedCallStacks["$key"]:=""}"
-        : "${all_init_execs_json["$key"]:="[]"}"
-        : "${all_init_opens_json["$key"]:="[]"}"
-        : "${all_init_endpoints_json["$key"]:="[]"}"
-        : "${all_init_rules_json["$key"]:="[]"}"
-        : "${all_init_syscalls["$key"]:=""}"
-        : "${all_init_capabilities["$key"]:=""}"
-        : "${superset_init_syscalls["$key"]:=[]}"
-        : "${superset_init_capabilities["$key"]:=""}"
-        : "${superset_init_execs["$key"]:=""}"
-        : "${superset_init_open["$key"]:=""}"
-        : "${superset_init_endpoints["$key"]:=""}"
-        : "${superset_init_rules["$key"]:=""}"
-        : "${initkeys["$key"]:=""}"
-        initkeys["$key"]="${key}"
-          init_imageID["$key"]=$(yq ".spec.initContainers[$i].imageID" "$norm_file" 2>/dev/null || true)
-          init_imageID["$key"]=$(echo "${init_imageID["$key"]}" | sed 's|^docker-pullable://|docker.io/|')
-          init_imageTag["$key"]=$(yq ".spec.initContainers[$i].imageTag" "$norm_file" 2>/dev/null || true)
-          init_containerName["$key"]=$(yq ".spec.initContainers[$i].name" "$norm_file" 2>/dev/null || true)
-          init_identifiedCallStacks["$key"]=$(yq ".spec.initContainers[$i].identifiedCallStacks" "$norm_file" 2>/dev/null || true)
+        initcontainerName=$(yq -r ".spec.initContainers[$j].name // \"\" " "$norm_file" 2>/dev/null || true)
+        initkey="${shortname}${initcontainerName}" 
+        #echo $initkey "for initContainer"
+        : "${init_imageID["$initkey"]:=""}"
+        : "${init_imageTag["$initkey"]:=""}"
+        : "${init_containerName["$initkey"]:=""}"
+        : "${init_identifiedCallStacks["$initkey"]:=""}"
+        : "${all_init_execs_json["$initkey"]:="[]"}"
+        : "${all_init_opens_json["$initkey"]:="[]"}"
+        : "${all_init_endpoints_json["$initkey"]:="[]"}"
+        : "${all_init_rules_json["$initkey"]:="[]"}"
+        : "${all_init_syscalls["$initkey"]:=""}"
+        : "${all_init_capabilities["$initkey"]:=""}"
+        : "${superset_init_syscalls["$initkey"]:=[]}"
+        : "${superset_init_capabilities["$initkey"]:=""}"
+        : "${superset_init_execs["$initkey"]:=""}"
+        : "${superset_init_open["$initkey"]:=""}"
+        : "${superset_init_endpoints["$initkey"]:=""}"
+        : "${superset_init_rules["$initkey"]:=""}"
+        : "${initkeys["$initkey"]:=""}"
+        initkeys["$initkey"]="${initkey}"
+          init_imageID["$initkey"]=$(yq ".spec.initContainers[$j].imageID" "$norm_file" 2>/dev/null || true)
+          init_imageID["$initkey"]=$(echo "${init_imageID["$initkey"]}" | sed 's|^docker-pullable://|docker.io/|')
+          init_imageTag["$initkey"]=$(yq ".spec.initContainers[$j].imageTag" "$norm_file" 2>/dev/null || true)
+          init_containerName["$initkey"]=$(yq ".spec.initContainers[$j].name" "$norm_file" 2>/dev/null || true)
+          init_identifiedCallStacks["$initkey"]=$(yq ".spec.initContainers[$j].identifiedCallStacks" "$norm_file" 2>/dev/null || true)
 
-          init_syscalls=$(yq ".spec.initContainers[$i].syscalls[]" "$norm_file" 2>/dev/null || true)
-          init_capabilities=$(yq ".spec.initContainers[$i].capabilities[]" "$norm_file" 2>/dev/null || true)
+          init_syscalls=$(yq ".spec.initContainers[$j].syscalls[]" "$norm_file" 2>/dev/null || true)
+          init_capabilities=$(yq ".spec.initContainers[$j].capabilities[]" "$norm_file" 2>/dev/null || true)
 
-          init_execs_json=$(yq -o=json ".spec.initContainers[$i].execs" "$norm_file" 2>/dev/null || echo "[]")
-          init_opens_json=$(yq -o=json ".spec.initContainers[$i].opens" "$norm_file" 2>/dev/null || echo "[]")
-          init_endpoints_json=$(yq -o=json ".spec.initContainers[$i].endpoints" "$norm_file" 2>/dev/null || echo "[]")
-          init_rules_json=$(yq -o=json ".spec.initContainers[$i].rulePolicies | select(. != null) | to_entries" "$norm_file" 2>/dev/null || echo "[]")
+          init_execs_json=$(yq -o=json ".spec.initContainers[$j].execs" "$norm_file" 2>/dev/null || echo "[]")
+          init_opens_json=$(yq -o=json ".spec.initContainers[$j].opens" "$norm_file" 2>/dev/null || echo "[]")
+          init_endpoints_json=$(yq -o=json ".spec.initContainers[$j].endpoints" "$norm_file" 2>/dev/null || echo "[]")
+          init_rules_json=$(yq -o=json ".spec.initContainers[$j].rulePolicies | select(. != null) | to_entries" "$norm_file" 2>/dev/null || echo "[]")
 
-          all_init_execs_json["$key"]=$(jq -s 'add | unique_by(.path)' <(echo "${all_init_execs_json["$key"]}") <(echo "$init_execs_json"))
-          all_init_opens_json["$key"]=$(jq -s 'add | unique_by(.path)' <(echo "${all_init_opens_json["$key"]}")  <(echo "$init_opens_json") | normalize_opens  | collapse_opens_with_globs | collapse_opens_events)
-          all_init_endpoints_json["$key"]=$(jq -s 'add | unique' <(echo "${all_init_endpoints_json["$key"]}") <(echo "$init_endpoints_json"))
-          all_init_rules_json["$key"]=$(jq -s 'map(. // []) | add' <(echo "${all_init_rules_json["$key"]}") <(echo "$init_rules_json"))
+          all_init_execs_json["$initkey"]=$(jq -s 'add | unique_by(.path)' <(echo "${all_init_execs_json["$key"]:-[]}") <(echo "$init_execs_json"))
+          all_init_opens_json["$initkey"]=$(jq -s 'add | unique_by(.path)' <(echo "${all_init_opens_json["$key"]:-[]}")  <(echo "$init_opens_json") | normalize_opens  | collapse_opens_with_globs | collapse_opens_events)
+          all_init_endpoints_json["$initkey"]=$(jq -s 'add | unique' <(echo "${all_init_endpoints_json["$key"]:-[]}") <(echo "$init_endpoints_json"))
+          all_init_rules_json["$initkey"]=$(jq -s 'map(. // []) | add' <(echo "${all_init_rules_json["$key"]:-[]}") <(echo "$init_rules_json"))
 
-        all_init_syscalls["$key"]="${all_init_syscalls["$key"]} $init_syscalls"
-        all_init_capabilities["$key"]="${all_init_capabilities["$key"]} $init_capabilities"
+        all_init_syscalls["$initkey"]="${all_init_syscalls["$initkey"]} $init_syscalls"
+        all_init_capabilities["$initkey"]="${all_init_capabilities["$initkey"]} $init_capabilities"
 
-    superset_init_syscalls["$key"]=$(printf "%s\n" ${all_init_syscalls["$key"]} | sort -u)
-    superset_init_capabilities["$key"]=$(printf "%s\n" ${all_init_capabilities["$key"]} | sort -u)
+    superset_init_syscalls["$initkey"]=$(printf "%s\n" ${all_init_syscalls["$initkey"]} | sort -u)
+    superset_init_capabilities["$initkey"]=$(printf "%s\n" ${all_init_capabilities["$initkey"]} | sort -u)
 
   
-    superset_init_execs["$key"]=$(echo "${all_init_execs_json["$key"]}" | yq -P '.')
-    superset_init_open["$key"]=$(echo "${all_init_opens_json["$key"]}" | yq -P '.')
-    superset_init_endpoints["$key"]=$(echo "${all_init_endpoints_json["$key"]}" | yq -P '.' )
-    superset_init_rules["$key"]=$(echo "${all_init_rules_json["$key"]}" | jq '
+    superset_init_execs["$initkey"]=$(echo "${all_init_execs_json["$initkey"]}" | yq -P '.')
+    superset_init_open["$initkey"]=$(echo "${all_init_opens_json["$initkey"]}" | yq -P '.')
+    superset_init_endpoints["$initkey"]=$(echo "${all_init_endpoints_json["$initkey"]}" | yq -P '.' )
+    superset_init_rules["$initkey"]=$(echo "${all_init_rules_json["$initkey"]:-[]}" | jq '
       group_by(.key) | map({
         key: .[0].key, value: (map(.value.processAllowed // []) | add | unique | if length > 0 then {processAllowed: .} else {} end)
       }) | from_entries' | yq -P '.')
@@ -403,6 +427,8 @@ spec:
   - $architecture
   containers:
 EOF
+echo "Container keys for $shortname: ${!keys[@]}"
+echo "InitContainer keys for $shortname: ${!initkeys[@]}"
 ## Now loop over all container keys
 for key in "${!keys[@]}"; do
 cat <<EOF >> "$OUTPUT_FILE"
@@ -434,41 +460,41 @@ if [ "$init_container_count" -gt 0 ];  then
 cat <<EOF >> "$OUTPUT_FILE"
   initContainers:
 EOF
-for key in "${!initkeys[@]}"; do
+for initkey in "${!initkeys[@]}"; do
 cat <<EOF >> "$OUTPUT_FILE"
-  - $(print_yaml_list_or_null_inline "capabilities" ${superset_init_capabilities["$key"]})
-$(if [ "$(echo "${all_init_endpoints_json["$key"]}" | jq -r 'length')" -eq 0 ]; then
+  - $(print_yaml_list_or_null_inline "capabilities" ${superset_init_capabilities["$initkey"]})
+$(if [ "$(echo "${all_init_endpoints_json["$initkey"]:-[]}" | jq -r 'length')" -eq 0 ]; then
     echo "    endpoints: null"
   else
     echo "    endpoints:"
-    echo "${superset_init_endpoints["$key"]}" | indent_endpoints | sed -E '/^[[:space:]]*$/d'
+    echo "${superset_init_endpoints["$initkey"]}" | indent_endpoints | sed -E '/^[[:space:]]*$/d'
   fi)
-$(if [ "$(echo "${all_init_execs_json["$key"]}" | jq -r 'length')" -eq 0 ]; then
+$(if [ "$(echo "${all_init_execs_json["$initkey"]:-[]}" | jq -r 'length')" -eq 0 ]; then
     echo "    execs: null"
   else
     echo "    execs:"
-    echo "${superset_init_execs["$key"]}" | indent_execs | sed -E '/^[[:space:]]*$/d'
+    echo "${superset_init_execs["$initkey"]}" | indent_execs | sed -E '/^[[:space:]]*$/d'
   fi)
-    identifiedCallStacks: ${init_identifiedCallStacks["$key"]}
-    imageID: ${init_imageID["$key"]}
-    imageTag: ${init_imageTag["$key"]}
-    name: ${init_containerName["$key"]}
-$(if [ -z "${all_init_opens_json["$key"]}" ]; then
+    identifiedCallStacks: ${init_identifiedCallStacks["$initkey"]}
+    imageID: ${init_imageID["$initkey"]}
+    imageTag: ${init_imageTag["$initkey"]}
+    name: ${init_containerName["$initkey"]}
+$(if [ -z "${all_init_opens_json["$initkey"]}" ]; then
     echo "    opens: null"
   else
     echo "    opens:"
-    echo "${superset_init_open["$key"]}" | indent_opens | sed -E '/^[[:space:]]*$/d'
+    echo "${superset_init_open["$initkey"]}" | indent_opens | sed -E '/^[[:space:]]*$/d'
   fi)
-$(if [ "$(echo "${all_init_rules_json["$key"]:-[]}" | jq -r 'length')" -eq 0 ]; then #TODO: fix if null
+$(if [ "$(echo "${all_init_rules_json["$initkey"]:-[]}" | jq -r 'length')" -eq 0 ]; then 
     echo "    rulePolicies: {}"
     else
     echo "    rulePolicies:"
-    echo "${superset_init_rules["$key"]}" | indent_rules
+    echo "${superset_init_rules["$initkey"]}" | indent_rules
   fi)
     seccompProfile:
       spec:
         defaultAction: ""
-$(print_yaml_list_or_null_inline "    syscalls" ${superset_init_syscalls["$key"]})
+$(print_yaml_list_or_null_inline "    syscalls" ${superset_init_syscalls["$initkey"]})
 EOF
 done
 fi
