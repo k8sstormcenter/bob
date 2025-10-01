a8b7aaf543d2e4fa0ce363e6dc47b7f3dcdf59fc
entlein <einentlein@gmail.com>
2025-10-01 20:03:14 +0200

    typo in bobapply script
    Signed-off-by: entlein <einentlein@gmail.com>

--- src/bashhelpers/bobapply.sh
+++ src/bashhelpers/bobapply.sh
@@ -4,11 +4,25 @@ for f in $1/*_bob.yaml; do
   shortname=$(basename "$f" | sed 's/_bob.yaml$//')
   kind=$(echo "$shortname" | cut -d'-' -f1)
   name=$(echo "$shortname" | cut -d'-' -f2-)
-  if [ "$kind" = "replicaset" ]; then
-    2kind="deployment"
-    kubectl label --overwrite -n harbor "$2kind/$name" kubescape.io/user-defined-profile="$shortname"
-  fi
-  echo $shortname $kind $name
   kubectl apply -f $f 
+  # Label deployment if kind is replicaset
+  if [ "$kind" = "replicaset" ]; then
+    kubectl label --overwrite -n harbor "deployment/$name" kubescape.io/user-defined-profile="$shortname"
+    # Also label all pods belonging to the replicaset
+    pods=$(kubectl get pods -n harbor -l "app.kubernetes.io/name=$name" -o jsonpath='{.items[*].metadata.name}')
+    for pod in $pods; do
+      kubectl label --overwrite -n harbor pod/$pod kubescape.io/user-defined-profile="$shortname"
+    done
+  fi
+
+  # Label pods for jobs or other resources that change templatehash
+  if [ "$kind" = "job" ] || [ "$kind" = "replicaset" ]; then
+    pods=$(kubectl get pods -n harbor -l "job-name=$name" -o jsonpath='{.items[*].metadata.name}')
+    for pod in $pods; do
+      kubectl label --overwrite -n harbor pod/$pod kubescape.io/user-defined-profile="$shortname"
+    done
+  fi
+
+  echo $shortname $kind $name
   kubectl label --overwrite -n harbor "$kind/$name" kubescape.io/user-defined-profile="$shortname"
 done
